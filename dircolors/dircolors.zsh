d294b78aa36ff82a957d15523c36d06e08e89eb5
fujimogn <fujimogn@gmail.com>
2011-09-12 10:24:12 +0900

    add submodule dircolors/dircolors

---
+++ dircolors/dircolors.zsh
@@ -0,0 +1,22 @@
+
+
+case ${OSTYPE} in
+  darwin*)
+    alias ls="ls -FG"
+    ;;
+  *)
+    alias ls="ls -F --color=auto"
+    ;;
+esac
+
+if  which dircolors > /dev/null 2>&1 ; then
+  dircolors=${DOTDIR}/dircolors/dircolors/.dircolors-${terminfo[colors]}
+  if [[ -f $dircolors ]]; then
+    eval $(dircolors -b $dircolors)
+  else
+    eval $(dircolors)
+  fi
+  # overwide ls for darwin
+  alias ls="ls -F --color=auto"
+fi
+
