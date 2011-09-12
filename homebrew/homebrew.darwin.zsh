aecf88df3b081970836c6c0ce0f738c347d2ce80
fujimogn <fujimogn@gmail.com>
2011-09-12 11:55:37 +0900

    fix

--- homebrew/homebrew.darwin.zsh
+++ homebrew/homebrew.darwin.zsh
@@ -1,7 +1,7 @@
 #!/usr/bin/zsh
 #
 # $File: ${DOTDIR}/homebrew/homebrew.darwin.zsh
-# $Date: 2011-09-12T03:29:14+0900$
+# $Date: 2011-09-12T11:55:21+0900$
 # vim:filetype=zsh:tabstop=2:shiftwidth=2:fdm=marker:
 
 ! which brew >/dev/null 2>&1 && return
@@ -11,7 +11,7 @@ export HOMEBREW_USE_GCC=1
 export HOMEBREW_LIST_PATH=${DOTDIR}/homebrew
 alias brews='brew list -1'
 
-function _brewlist() {
+function brewlist() {
 
   local target\
         cmd\
@@ -34,6 +34,10 @@ function _brewlist() {
   target=${target:-${HOMEBREW_LIST_PATH:-$HOME}/${rcname}}
   cmd+=${cmd:-"brew list -1 > \$target"}
   eval `echo $cmd`
+}
+
+brew_fix_chmod() {
+  sudo chown -R $USER `brew --prefix`
 }
 
 
