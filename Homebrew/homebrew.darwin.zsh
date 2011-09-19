288296fcb8c6ab4ff188444ba93bbe62cca3d483
fujimogn <fujimogn@gmail.com>
2011-09-20 08:12:23 +0900

    fixed

--- Homebrew/homebrew.darwin.zsh
+++ Homebrew/homebrew.darwin.zsh
@@ -1,10 +1,10 @@
 #!/usr/bin/zsh
 #
 # $File: ${DOTDIR}/homebrew/homebrew.darwin.zsh
-# $Date: 2011-09-17T03:32:20+0900$
+# $Date: 2011-09-20T08:12:03+0900$
 # vim:filetype=zsh:tabstop=2:shiftwidth=2:fdm=marker:
 
-! which brew >/dev/null 2>&1 && return
+! which brew >/dev/null 2>&1 && return 1
 
 export PATH="$(brew --prefix)/bin:$PATH"
 export HOMEBREW_USE_GCC=1
--- vim/vim.zsh
+++ vim/vim.zsh
@@ -1,27 +1,11 @@
 #!/usr/bin/zsh
 #
 # $File: ${DOTDIR}/vim/vim.zsh
-# $Date: 2011-09-17T08:31:48+0900$
+# $Date: 2011-09-20T08:10:15+0900$
 # vim:filetype=zsh:tabstop=2:shiftwidth=2:fdm=marker:
 
-! which vim >/dev/null 2>&1 && return
+! which vim >/dev/null 2>&1 && return 1
 
 export EDITOR=vim
 export VISUAL=vim
-
-function vundle() {
-  if [ -n $1 ]; then
-    case $1 in
-      "update")
-        vundle-init
-        vim -c "execute \"BundleInstall!\" | q | q"
-        ;;
-      *) echo "unknown option$1" 2
-        ;;
-    esac
-  else
-    vundle-init
-    vim -c "execute \"BundleInstall\" | q | q"
-  fi
-}
 
