fb964e457941bd10e4ee0abd526b8451ba434f13
fujimogn <fujimogn@gmail.com>
2011-10-15 09:11:05 +0900

    init commit

---
+++ vim/vim.cygwin.zsh
@@ -0,0 +1,10 @@
+#!/usr/bin/zsh
+#
+# $File: ${DOTDIR}/vim/vim.darwin.zsh
+# $Date: 2011-09-20T08:09:13+0900$
+# vim:filetype=zsh:tabstop=2:shiftwidth=2:fdm=marker:
+
+! which vim >/dev/null 2>&1 && return 1
+
+alias vi=vim
+alias v=vim
