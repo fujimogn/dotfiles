003893c809278e88f46e6bfe0cdab5d98c0bbc73
fujimogn <fujimogn@gmail.com>
2012-02-20 07:37:51 +0900

    changed keybind  Please enter the commit message for your changes. Lines starting

--- tmux/tmux.zsh
+++ tmux/tmux.zsh
@@ -1,16 +1,17 @@
 #!/usr/bin/zsh
 #
 # $File: ${DOTDIR}/tmux/tmux.zsh
-# $Date: 2011-11-01T08:36:01+0900$
+# $Date: 2012-02-20T04:19:50+0900$
 # vim:filetype=zsh:tabstop=2:shiftwidth=2:fdm=marker:
 
 ! which tmux >/dev/null 2>&1 && return 1
 
-alias tm='tmux'
-alias tma="tmux attach"
-alias tmls='tmux list-sessions'
-alias tmlw='tmux list-window'
+alias t='tmux'
+alias ta='tmux attach -t'
+alias tls='tmux list-sessions'
+alias tlw='tmux list-window'
 
 function tmux-edit-rc {
   ${EDITOR-vi} ${DOTDIR}/tmux/.tmux.conf
 }
+
