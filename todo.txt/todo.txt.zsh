e6b86646fe5a212da7c87c0cb4faf6ebb5e1d5a1
fujimogn <fujimogn@gmail.com>
2012-12-17 16:28:01 +0900

    alias 修正

--- todo.txt/todo.txt.zsh
+++ todo.txt/todo.txt.zsh
@@ -1,12 +1,12 @@
 #!/bin/zsh
 #
 # $File: ${DOTDIR}/todo.txt/todo.txt.zsh
-# $Date: 2012-02-23T12:16:02+0900$
+# $Date: 2012-12-16T00:21:06+0900$
 # vim:filetype=zsh:tabstop=2:shiftwidth=2:fdm=marker:
 
 ! which todo.sh >/dev/null 2>&1 && return 1
 
-function td() {
+function t() {
   if [ $# -eq 0 ]; then
     todo.sh ls
   else
@@ -16,8 +16,8 @@ function td() {
 
 # compdef _todo.sh t
 
-alias tdn="td ls +next"
-alias tda="td add"
+alias tn="td ls +next"
+alias ta="td add"
 
 function todo-edit-rc {
   ${EDITOR-vi} ${DOTDIR}/todo.txt/.todo.cfg
